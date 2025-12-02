59f9c5367da1b19aee3006da7ce3037c5419ded4
manepallikotisai <111281655+kotisai1234578@users.noreply.github.com>
2025-12-03 02:10:05 +0530

    Delete SQL/1.sql

--- SQL/1.sql
+++
@@ -1,9 +0,0 @@
-SELECT b.user_id,
-       b.room_no
-FROM bookings b
-JOIN (
-  SELECT user_id, MAX(booking_date) AS last_booking_date
-  FROM bookings
-  GROUP BY user_id
-) last_b ON b.user_id = last_b.user_id
-       AND b.booking_date = last_b.last_booking_date;
